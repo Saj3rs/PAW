560243a52db38c25c62ca9f15d49f49cfbf9b7df
Damian F <damfig@interia.eu>
2023-03-20 20:19:53 +0100

    Second commit
    Testing xammp

---
+++ Setup/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+   port :8080
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Test Xammp'a</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
